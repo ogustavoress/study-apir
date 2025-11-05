eeb2d383cb118c7ee2741aedfa3c85461470a2ef
Gustavo <ogustavoress@gmail.com>
2025-11-05 10:11:53 -0300

    feat: adicionando curl para deletar produto

--- server/run-integration-tests.sh
+++ server/run-integration-tests.sh
@@ -17,6 +17,7 @@ HTTP_STATUS=$(
     }'
 )
 
+echo "Status HTTP: $HTTP_STATUS"
 if [ "$HTTP_STATUS" -ne 201 ]; then
     echo "Erro ao criar produto"
     exit 1
@@ -25,6 +26,18 @@ fi
 PRODUTO_ID=$(jq '.id' product_create.json)
 
 echo "Produto criado com ID: $PRODUTO_ID"
+
+HTTP_STATUS=$(
+    curl -X 'DELETE' \
+    "http://localhost:9000/api/v3/produtos/$PRODUTO_ID" \
+    -H 'accept: */*' \
+    -w "%{http_code}"
+)
+echo "Status HTTP: $HTTP_STATUS"
+if [ "$HTTP_STATUS" -ne 204 ]; then
+    echo "Erro ao deletar produto"
+    exit 1
+fi
 
 HTTP_STATUS=$(curl -X GET 'http://localhost:9000/api/v3/produtos' -o product_list.json -w "%{http_code}" -H 'accept: */*')
 echo "Status HTTP: $HTTP_STATUS"
